eaf53bda542f306f1c2ac55fb5b0e1459e8f04af
hurter3 <mark.hurter@hotmail.co.uk>
2020-05-07 11:12:53 +0000

    add env conditional

--- LionsTour/settings.py
+++ LionsTour/settings.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-# import env
+from os import path
+if path.exists("env.py"):
+  import env 
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -24,8 +26,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['lionstour2021sa.herokuapp.com']
+if path.exists("env.py"):
+  ALLOWED_HOSTS = []
+else:
+  ALLOWED_HOSTS = ['lionstour2021sa.herokuapp.com']
 
 # Application definition
 
