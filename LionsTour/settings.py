595757f294f2d7457971f966b73c4ab6c6dbf03e
hurter3 <mark.hurter@hotmail.co.uk>
2020-05-07 09:44:16 +0000

    remove env

--- LionsTour/settings.py
+++ LionsTour/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+# import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -24,8 +24,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['lionstour2021sa.herokuapp.com']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lionstour2021sa.herokuapp.com']
 
 # Application definition
 
