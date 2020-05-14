db1aca0aa7c3425ed306c881e8bbe17471a29ecb
hurter3 <mark.hurter@hotmail.co.uk>
2020-05-14 19:14:44 +0000

    added competition model

---
+++ competition/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+import datetime
+from django.contrib.auth.models import User
+
+class Competition(models.Model):
+
+    customer = models.ForeignKey(User, on_delete=models.PROTECT)
+    match1 = models.CharField(max_length=254, default='')
+    sascore1 = models.IntegerField()
+    lionsscore1 = models.IntegerField()
+    submit_date = models.DateField(default=datetime.date.today, null=True)
+
+    
+
+    def __str__(self):
+        return f'{self.id}-{self.customer.username}'
