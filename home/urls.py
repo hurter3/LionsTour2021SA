0e0956ecf7dd6c7dc04b8e6f169ad97743314f5b
hurter3 <mark.hurter@hotmail.co.uk>
2020-05-14 13:42:53 +0000

    made navbar and styling changes with links eing connect to dummy url and html pages

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
-from .views import home_view, history_view, photos_view, tickets_view
+from .views import home_view, history_view, photos_view, fixtures_view, tickets_view
 
 
 urlpatterns = [
-    url(r'^$', home_view, name='index'),
+    url(r'^$', home_view, name='home'),
     url('history/', history_view, name="history"),
- ]
+    url('photos/', photos_view, name="photos"),
+    url('fixtures/', fixtures_view, name="fixtures"),
+    url('tickets/', tickets_view, name="tickets"),
+]
--- home/views.py
+++ home/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 
 def home_view(request):
-    return render(request, "index.html")
+    return render(request, "home.html")
 
 def history_view(request):
     return render(request, "history.html", {"home": "history"})
 
 def photos_view(request):
-    return render(request, "photos.html")
+    return render(request, "photos.html", {"home": "photos"})
+
+def fixtures_view(request):
+    return render(request, "fixtures.html", {"home": "fixtures"})
 
 def tickets_view(request):
-    return render(request, "tickets.html")
+    return render(request, "tickets.html", {"home": "tickets"})
