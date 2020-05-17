4da4905366673c7adfcf63c16fcfc07c04dd8ed4
hurter3 <mark.hurter@hotmail.co.uk>
2020-05-17 09:15:20 +0000

    added class ListView for products

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_products
+#from .views import all_products, ProductListView
+from .views import ProductListView
 
 urlpatterns = [
-    url(r'^$', all_products, name='products'),
+# url(r'^$', all_products, name='products'),
+ url(r'^$', ProductListView.as_view(), name='product-list'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Product
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
-def all_products(request):
-    products = Product.objects.all()
-    return render(request, "products.html", {"products": products})
+#def all_products(request):
+#    products = Product.objects.all()
+#    return render(request, "products.html", {"products": products})
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    model = Product
+    template_name = 'products.html'
+    context_object_name = 'products'
+
