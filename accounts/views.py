4a77878ff54668a615bbac47d7c06d7a33db26af
hurter3 <mark.hurter@hotmail.co.uk>
2020-06-02 16:34:26 +0000

    pushing code to fix travis

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ def register_view(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
+            messages.success(request, 'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
