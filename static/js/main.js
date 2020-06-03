53c9023030d2418521ed2039e37160027401e9fa
hurter3 <mark.hurter@hotmail.co.uk>
2020-06-03 10:54:51 +0000

    expanded on the home page to include a carousal

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    function changeChevronDirection() {
+        if ($('.chevron').hasClass('fa-chevron-down')) {
+            $('.chevron').addClass('fa-chevron-up').removeClass('fa-chevron-down');
+        } else {
+            $('.chevron').addClass('fa-chevron-down').removeClass('fa-chevron-up');
+        }
+    }
