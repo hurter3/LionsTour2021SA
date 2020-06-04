dcee0a374e198e7e2409cb9bef2d277d10960255
hurter3 <mark.hurter@hotmail.co.uk>
2020-06-04 12:33:05 +0000

    change cart.html format and updated css styling

--- cart/views.py
+++ cart/views.py
@@ -24,7 +24,7 @@ def add_to_cart(request, id):
         cart[id] = cart.get(id, quantity) 
 
     request.session['cart'] = cart
-    return redirect(reverse('product-list-all'))
+    return redirect(reverse('view_cart'))
 
 
 def adjust_cart(request, id):
