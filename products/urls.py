c4536e149eefdb2cc814ef9965fa8b9a3344dcdd
hurter3 <mark.hurter@hotmail.co.uk>
2020-06-19 14:09:59 +0000

    created a generic view to pass through categories for shopping to remove similiar code being reused except for the filter

--- products/urls.py
+++ products/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls import url, include
 #from .views import all_products, ProductListView
-from .views import ProductListView, ProductDetailView, ProductListMenView, ProductListWomanView,ProductListChildrenView, ProductListSouvenirsView
+from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
 # url(r'^$', all_products, name='products'),
  url(r'^$', ProductListView.as_view(), name='product-list-all'),
- url(r'product/men', ProductListMenView.as_view(), name='product-list-men'),
- url(r'product/woman', ProductListWomanView.as_view(), name='product-list-woman'),
- url(r'product/children', ProductListChildrenView.as_view(), name='product-list-children'),
- url(r'product/souvenirs', ProductListSouvenirsView.as_view(), name='product-list-souvenirs'),
+# url(r'men/', ProductListView.as_view(), name='product-list-view'),
+#  url(r'men/', ProductListMenView.as_view(), name='product-list-men'),
+# url(r'woman/', ProductListWomanView.as_view(), name='product-list-woman'),
+# url(r'children/', ProductListChildrenView.as_view(), name='product-list-children'),
+# url(r'souvenirs/', ProductListSouvenirsView.as_view(), name='product-list-souvenirs'),
 
  url(r'(?P<pk>\d+)/', ProductDetailView.as_view(), name='product-detail'),
 
-# url(r'(?P<category>[\w\-]+)/', ProductListView.as_view(), name='product-list-view'),
+ url(r'(?P<category>[\w\-]+)/', ProductListView.as_view(), name='product-list-view'),
 # for navbat li link <a class="dropdown-item" href="{% url 'product-list-view' category='Men' %}">Men's</a>
 ]
--- products/views.py
+++ products/views.py
@@ -2,38 +2,42 @@ from django.shortcuts import render
 from .models import Product
 from django.views.generic import DetailView, ListView
 
-class ProductListView(ListView):
-    paginate_by = 4
-    queryset = Product.objects.all()
-    model = Product
-    template_name = 'products.html'
-    context_object_name = 'products'
+#class ProductListView(ListView):
+#    paginate_by = 4
+#    queryset = Product.objects.all()
+#    model = Product
+#    template_name = 'products.html'
+#    context_object_name = 'products'
 
 
-class ProductListMenView(ListView):
-   queryset = Product.objects.all().filter(category="Men")
-   model = Product
-   template_name = 'products.html'
-   context_object_name = 'products'
+#class ProductListMenView(ListView):
+#    paginate_by = 4
+#    queryset = Product.objects.all().filter(category="Men")
+#    model = Product
+#    template_name = 'products.html'
+#    context_object_name = 'products'
 
-class ProductListWomanView(ListView):
-   queryset = Product.objects.all().filter(category="Woman")
-   model = Product
-   template_name = 'products.html'
-   context_object_name = 'products'
+#class ProductListWomanView(ListView):
+#    paginate_by = 4
+#    queryset = Product.objects.all().filter(category="Woman")
+#    model = Product
+#    template_name = 'products.html'
+#    context_object_name = 'products'
 
-class ProductListChildrenView(ListView):
-   queryset = Product.objects.all().filter(category="Children")
-   model = Product
-   template_name = 'products.html'
-   context_object_name = 'products'
+#class ProductListChildrenView(ListView):
+#    paginate_by = 4
+#    queryset = Product.objects.all().filter(category="Children")
+#    model = Product
+#    template_name = 'products.html'
+#    context_object_name = 'products'
 
-class ProductListSouvenirsView(ListView):
-   queryset = Product.objects.all().filter(category="Souvenirs")
-   model = Product
-   template_name = 'products.html'
-   context_object_name = 'products'
-   
+#class ProductListSouvenirsView(ListView):
+#    paginate_by = 4
+#    queryset = Product.objects.all().filter(category="Souvenirs")
+#    model = Product
+#    template_name = 'products.html'
+#    context_object_name = 'products'
+    
 class ProductDetailView(DetailView):
     queryset = Product.objects.all()
     model = Product
@@ -41,14 +45,15 @@ class ProductDetailView(DetailView):
     context_object_name = 'products'
 
 
-#class ProductListView(ListView):
-#    model = Product
-#    template_name = 'products.html'
-#    context_object_name = 'products'
+class ProductListView(ListView):
+    paginate_by = 4
+    model = Product
+    template_name = 'products.html'
+    context_object_name = 'products'
 
-#    def get_queryset(self):
-#        if self.kwargs['category'] == 'All':
-#            return Product.objects.all()
-#        else:
-#            return Product.objects.filter(category=self.kwargs['category'])
+    def get_queryset(self):
+        if self.kwargs['category'] == 'All':
+            return Product.objects.all()
+        else:
+            return Product.objects.filter(category=self.kwargs['category'])
 
