c64415ac8b23b92f98675f9e2f338f74f66db145
hurter3 <mark.hurter@hotmail.co.uk>
2020-07-14 14:56:55 +0000

    correct typo as dj_database_url was not loaded

--- LionsTour/settings.py
+++ LionsTour/settings.py
@@ -11,7 +11,9 @@ import os
 from os import path
 if path.exists("env.py"):
     import env 
-    import dj_database_url
+
+import dj_database_url 
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
